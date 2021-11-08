004cbdb9fb50cc9e8f05f9f246346a08e108a998
= <zdc6@njit.edu>
2021-11-08 00:27:02 -0500

    Major changes

--- paper1method.py
+++ paper1method.py
@@ -1,13 +1,27 @@
 # Method from this paper: https://www.researchgate.net/publication/224180108_Solving_Sudoku_with_genetic_operations_that_preserve_building_blocks
 import random
+import modified_pyeasyga
 import numpy as np
+import time
 
 # Each array is a box of the sudoku
 # The first box is top left, the second is middle top, etc.
 DIM = 9
 SQRT_DIM = int(DIM**0.5)
+random.seed(time.time())
+easy_board = [0,0,0,8,0,0,0,0,7,3,0,0,0,4,7,0,0,0,8,0,1,0,0,2,3,
+6,0,0,6,0,0,0,8,0,0,2,0,0,5,9,2,4,7,0,0,2,0,0,6,0,0,0,3,0,0,8,4,7,0,0,2,0,1,
+0,0,0,2,1,0,0,0,8,5,0,0,0,0,6,0,0,0]
+easy_board = [[0,0,4,6,7,0,5,0,8],[8,0,0,9,0,0,0,3,0],
+              [0,1,7,0,0,0,0,0,4],[3,0,0,0,6,9,0,0,1],
+              [7,4,0,0,0,0,0,6,9],[1,0,0,7,8,0,0,0,5],
+              [1,0,0,0,0,0,2,4,0],[0,8,0,0,0,6,0,0,1],
+              [3,0,6,0,9,1,5,0,0]]
+hard_board = [[7,5,0,0,1,0,4,0,0],[2,0,0,0,9,6,0,5,0],[0,0,0,0,0,4,0,0,3],
+              [0,0,0,0,0,7,5,0,1],[0,3,0,0,4,0,0,0,2],[0,0,5,0,3,0,9,0,0],
+              [9,0,0,0,0,0,0,0,4],[0,0,0,0,6,0,0,0,8],[3,8,7,0,0,0,5,1,0]]
 
-def generate_initial_boxes(given_puzzle):
+def generate_initial_boxes(given_puzzle, DIM=9, SQRT_DIM=3):
   board = []
   counted = set()
   for i in range(DIM * 2):
@@ -27,17 +41,17 @@ def generate_initial_boxes(given_puzzle):
       counted = set()
   return board
 
-def fitness_rows(board, row):
+def fitness_rows(board, row, DIM=9, SQRT_DIM=3):
   fitness = []
   if row:
     for i in range(SQRT_DIM):
       fitness.append(fitness_row(board[SQRT_DIM*i:(i+1)*SQRT_DIM]))
   else:
     for i in range(SQRT_DIM):
-      fitness.append(fitness_col(board[i*SQRT_DIM:(i+1)*SQRT_DIM]))
+      fitness.append(fitness_col(board[i::SQRT_DIM]))
   return fitness
 
-def fitness_col(group):
+def fitness_col(group, DIM=9, SQRT_DIM=3):
   fitness = 0
   for k in range(SQRT_DIM):
     unique_set = set()
@@ -47,7 +61,7 @@ def fitness_col(group):
     fitness += len(unique_set)
   return fitness
 
-def fitness_row(group):
+def fitness_row(group, DIM=9, SQRT_DIM=3):
   fitness = 0
   for i in range(SQRT_DIM):
     unique_set = set()
@@ -57,13 +71,15 @@ def fitness_row(group):
     fitness += len(unique_set)
   return fitness
 
-def reduced_fitness(board):
+def fitness_for_all(board, DIM=9, SQRT_DIM=3):
   fitness = 0
-  for col in board.T:
-    fitness += np.unique(col).size
+  for i in range(SQRT_DIM):
+    fitness += fitness_row(board[i*SQRT_DIM:(i+1)*SQRT_DIM])
+    fitness += fitness_col(board[i::SQRT_DIM])
+  return fitness
 
 
-def crossover(parent1, parent2):
+def crossover(parent1, parent2, DIM=9, SQRT_DIM=3):
   # Hard coded for 9x9 board for now; can be improved
   # TODO: Make this more general
   p1_fitness = fitness_rows(parent1, True)
@@ -71,8 +87,8 @@ def crossover(parent1, parent2):
   p2_col_fitness = fitness_rows(parent2, False)
   p1_col_fitness = fitness_rows(parent1, False)
 
-  child1 = np.zeros((DIM,DIM), dtype=int)
-  child2 = np.zeros((DIM,DIM), dtype=int)
+  child1 = [[],[],[],[],[],[],[],[],[]]
+  child2 = [[],[],[],[],[],[],[],[],[]]
 
   for i in range(SQRT_DIM):
     if p1_fitness[i] < p2_fitness[i]:
@@ -104,7 +120,7 @@ def crossover(parent1, parent2):
       child2[i+6] = selection[i+6]
   return child1, child2
 
-def modifiable_cell(board):
+def modifiable_cell(board, DIM=9, SQRT_DIM=3):
   # Return a list of sets with indexes of non-bound locations within box
   l = []
   for i in range(DIM):
@@ -114,18 +130,28 @@ def modifiable_cell(board):
         l[i].add(j)
   return l
 
-def mutate(gene, chance):
+MODIFIABLE_CELLS = modifiable_cell(easy_board)
+
+def mutate(lOfGenes, PROB=.3, DIM=9, SQRT_DIM=3):
   for i in range(DIM):
-    if random.random() < chance:
-      choice1 = random.choice(MODIFIABLE_CELLS[i])
-      # Ensure no duplicates
-      MODIFIABLE_CELLS[i].remove(choice1)
-      choice2 = random.choice(MODIFIABLE_CELLS[i])
-      # Restore the modifiables
-      MODIFIABLE_CELLS[i].add(choice1)
-      # Swap
-      gene[i][choice1], gene[i][choice2] = gene[i][choice2], gene[i][choice1]
-  return gene
+    to_add = []
+    for gene in lOfGenes:
+      if random.random() < PROB:
+        choice1 = random.choice([*MODIFIABLE_CELLS[i]])
+        # Ensure no duplicates
+        MODIFIABLE_CELLS[i].remove(choice1)
+        to_add.append(choice1)
+        choice2 = random.choice([*MODIFIABLE_CELLS[i]])
+        # Restore the modifiables
+        # Swap
+        gene[i][choice1], gene[i][choice2] = gene[i][choice2], gene[i][choice1]
+    for choice in to_add:
+      MODIFIABLE_CELLS[i].add(choice)
+  # return gene
+
+def selection(population):
+  return random.choice(population)
+
 
 
 test_boxes = [[1,2,3,4,5,6,7,8,9,],[4,5,6,7,8,9,1,2,3,],[7,8,9,1,2,3,4,5,6,]]
@@ -137,6 +163,28 @@ parent2 = np.array([np.array([9,8,7,6,5,4,3,2,1,]),np.array([8,7,6,5,4,3,2,1,9,]
            np.array([6,5,4,3,2,1,9,8,7,]),np.array([5,4,3,2,1,9,8,7,6,]),np.array([4,3,2,1,9,8,7,6,5,]),
            np.array([3,2,1,9,8,7,6,5,4,]),np.array([2,1,9,8,7,6,5,4,3,]),np.array([1,9,8,7,6,5,4,3,2,])])
 
-MODIFIABLE_CELLS = modifiable_cell(parent1)
+ga = modified_pyeasyga.GeneticAlgorithm(
+  seed_data = hard_board,
+  population_size=150,
+  generations = 10000,
+  crossover_probability = 0.3,
+  mutation_probability = 0.3,
+  elitism = True,
+)
+ga.tournament_size = 3
+ga.create_individual = generate_initial_boxes
+ga.fitness_function = fitness_for_all
+# ga.selection_function = selection
+ga.mutate_function = mutate
+ga.cross_over_function = crossover
+beg_time = time.time()
+ga.run()
+end_time = time.time()
 
-print(crossover(parent1, parent2))
+print(f'''{ga.best_individual()[0]} score in {ga.n_iterations} in generations in
+{end_time-beg_time} seconds  produces
+This board: {ga.best_individual()[1]}''')
+
+
+
+#print(crossover(parent1, parent2))
